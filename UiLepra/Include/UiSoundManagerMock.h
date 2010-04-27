7f5a1389b5b663cf2ae236ca0b7e5e6f195c9986
Jonas Bystroem <jonas.bystrom@loopon.se>
2010-04-27 08:42:09 +0200

    Did some debugging on sounds, ended up without any clues but dsound.dll crashing.

---
+++ UiLepra/Include/UiSoundManagerMock.h
@@ -0,0 +1,105 @@
+
+// Author: Jonas Byström
+// Copyright (c) 2002-2009, Righteous Games
+
+
+
+#pragma once
+
+#include "UiSoundManager.h"
+
+
+
+namespace UiLepra
+{
+
+
+
+class SoundManagerMock: public SoundManager
+{
+	typedef SoundManager Parent;
+public:
+	SoundManagerMock();
+	virtual ~SoundManagerMock();
+
+	SoundID LoadSound2D(const str& pFileName, LoopMode LoopMode, int pPriority);
+	SoundID LoadSound3D(const str& pFileName, LoopMode LoopMode, int pPriority);
+	SoundID LoadStream(const str& pFileName, LoopMode LoopMode, int pPriority);
+
+	void Release(SoundID pSoundID);
+
+	double GetStreamTime(SoundID pSoundID);
+
+	SoundInstanceID CreateSoundInstance(SoundID pSoundID);
+	void DeleteSoundInstance(SoundInstanceID pSoundIID);
+
+	bool Play(SoundInstanceID pSoundIID,
+			  float pVolume,
+			  float pPitch);
+
+	void Stop(SoundInstanceID pSoundIID);
+	void StopAll();
+	void TogglePause(SoundInstanceID pSoundIID);
+
+	bool IsPlaying(SoundInstanceID pSoundIID);
+	bool IsPaused(SoundInstanceID pSoundIID);
+
+	void SetPan(SoundInstanceID pSoundIID, float pPan);
+	void SetVolume(SoundInstanceID pSoundIID, float pVolume);
+	void SetPitch(SoundInstanceID pSoundIID, float pPitch);
+
+	void SetFrequency(SoundInstanceID pSoundIID, int pFrequency);
+	int GetFrequency(SoundInstanceID pSoundIID);
+
+	void SetListenerPosition(const Vector3DF& pPos, const Vector3DF& pVel,
+		const Vector3DF& pUp, const Vector3DF& pForward);
+
+	void SetDopplerFactor(float pFactor);
+	void SetRollOffFactor(float pFactor);
+
+	int GetChannel(SoundInstanceID pSoundIID);
+
+	void SetChorus(SoundInstanceID pSoundIID,
+				   int pFXIndex,
+				   float pDelay,
+				   float pFeedback,
+				   float pRate,
+				   float pDepth,		// Mod amount...
+				   float pWetness);
+
+	void SetFlanger(SoundInstanceID pSoundIID,
+					int pFXIndex,
+					float pDelay,
+					float pFeedback,
+					float pRate,
+					float pDepth,		// Mod amount...
+					float pWetness);
+
+	void SetCompressor(SoundInstanceID pSoundIID,
+					   int pFXIndex,
+					   float pRatio,
+					   float pThreshold,
+					   float pAttack,
+					   float pRelease);
+
+	void SetEcho(SoundInstanceID pSoundIID,
+				 int pFXIndex,
+				 float pFeedback,
+				 float pDelay,
+				 float pWetness);
+
+	void SetParamEQ(SoundInstanceID pSoundIID,
+					int pFXIndex,
+					float pCenter,
+					float pBandwidth,
+					float pGain);
+
+protected:
+	virtual void DoSetSoundPosition(SoundInstanceID pSoundIID, const Vector3DF& pPos, const Vector3DF& pVel);
+
+private:
+};
+
+
+
+}
---
+++ UiLepra/Source/UiSoundManagerMock.cpp
@@ -0,0 +1,141 @@
+
+// Author: Jonas Byström
+// Copyright (c) 2002-2009, Righteous Games
+
+
+
+#include "../Include/UiSoundManagerMock.h"
+
+
+
+namespace UiLepra
+{
+
+
+
+SoundManagerMock::SoundManagerMock()
+{
+}
+
+SoundManagerMock::~SoundManagerMock()
+{
+}
+
+SoundManager::SoundID SoundManagerMock::LoadSound2D(const str&, LoopMode, int )
+{
+	return (INVALID_SOUNDID);
+}
+
+SoundManager::SoundID SoundManagerMock::LoadSound3D(const str&, LoopMode, int)
+{
+	return (INVALID_SOUNDID);
+}
+
+SoundManager::SoundID SoundManagerMock::LoadStream(const str&, LoopMode, int)
+{
+	return (INVALID_SOUNDID);
+}
+
+void SoundManagerMock::Release(SoundID)
+{
+}
+
+double SoundManagerMock::GetStreamTime(SoundID)
+{
+	return (0);
+}
+
+SoundManager::SoundInstanceID SoundManagerMock::CreateSoundInstance(SoundID)
+{
+	return (INVALID_SOUNDINSTANCEID);
+}
+
+void SoundManagerMock::DeleteSoundInstance(SoundInstanceID)
+{
+}
+
+bool SoundManagerMock::Play(SoundInstanceID, float, float)
+{
+	return (false);
+}
+
+void SoundManagerMock::StopAll()
+{
+}
+
+void SoundManagerMock::Stop(SoundInstanceID)
+{
+}
+
+void SoundManagerMock::TogglePause(SoundInstanceID)
+{
+}
+
+bool SoundManagerMock::IsPlaying(SoundInstanceID)
+{
+	return (false);
+}
+
+bool SoundManagerMock::IsPaused(SoundInstanceID)
+{
+	return (false);
+}
+
+void SoundManagerMock::SetPan(SoundInstanceID, float)
+{
+}
+
+void SoundManagerMock::SetVolume(SoundInstanceID, float)
+{
+}
+
+void SoundManagerMock::SetPitch(SoundInstanceID, float)
+{
+}
+
+void SoundManagerMock::SetFrequency(SoundInstanceID, int)
+{
+}
+
+int SoundManagerMock::GetFrequency(SoundInstanceID)
+{
+	return (0);
+}
+
+void SoundManagerMock::SetDopplerFactor(float)
+{
+}
+
+void SoundManagerMock::SetRollOffFactor(float)
+{
+}
+
+void SoundManagerMock::SetChorus(SoundInstanceID, int, float, float, float, float, float)
+{
+}
+
+void SoundManagerMock::SetFlanger(SoundInstanceID, int, float, float, float, float, float)
+{
+}
+
+void SoundManagerMock::SetCompressor(SoundInstanceID, int, float, float, float, float)
+{
+}
+
+void SoundManagerMock::SetEcho(SoundInstanceID, int, float, float, float)
+{
+}
+
+void SoundManagerMock::SetParamEQ(SoundInstanceID, int, float, float, float)
+{
+}
+
+
+
+void SoundManagerMock::DoSetSoundPosition(SoundInstanceID, const Vector3DF&, const Vector3DF&)
+{
+}
+
+
+
+}
