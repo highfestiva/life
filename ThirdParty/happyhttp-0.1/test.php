d29d77764aff7258f3902e1b7bee995a82c2082e
Jonas <highfestiva@gmail.com>
2012-01-30 23:41:26 +0100

    Added some test code for happyhttp, will work!!!

---
+++ ThirdParty/happyhttp-0.1/test.php
@@ -0,0 +1,15 @@
+<?php
+// test.php - just a test script to dump out all GET and POST vars.
+?>
+<html>
+<head><title>vartest</title></head>
+<body>
+<pre>
+<?php
+print_r( $_REQUEST );
+?>
+</pre>
+</body>
+</html>
+
+
