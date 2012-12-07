29a6bf3e83916354ebefa109be17c83c2cb987c6
Jonas Byström <highfestiva@gmail.com>
2012-12-07 23:28:07 +0100

    - Fixed a bug in boolean RuntimeVariables (always set to false),  - made Push remove mass objects if performance too slow,  - Push also waits a bit longer for objects to load.

--- Push/Push.mm
+++ Push/Push.mm
@@ -154,6 +154,11 @@ using namespace Lepra;
 		bool lIsPressed = (lTouch.phase != UITouchPhaseEnded && lTouch.phase != UITouchPhaseCancelled);
 		PixelCoord lPreviousTap(lPrevTapPosition.y, _canvas->GetActualWidth() - lPrevTapPosition.x);
 		PixelCoord lThisTap(lTapPosition.y, _canvas->GetActualWidth() - lTapPosition.x);
+		if (_canvas && _canvas->GetOutputRotation() != 90)
+		{
+			lPreviousTap = PixelCoord(_canvas->GetActualHeight() - lPrevTapPosition.y, lPrevTapPosition.x);
+			lThisTap = PixelCoord(_canvas->GetActualHeight() - lTapPosition.y, lTapPosition.x);
+		}
 		Push::Push::GetApp()->mDragManager.UpdateDrag(lPreviousTap, lThisTap, lIsPressed);
 	}
 }
