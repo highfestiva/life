372356be7d5c72fe8ab0222786af13d47d7f3da4
Jonas <highfestiva@gmail.com>
2013-02-03 23:02:27 +0100

    Added (start of) OpenGL C-style shader support.

---
+++ UiTBC/Include/UiOpenGLShader.h
@@ -0,0 +1,41 @@
+
+// Author: Jonas Byström
+// Copyright (c) 2002-2009, Righteous Games
+
+
+
+#pragma once
+
+#include "UiOpenGLRenderer.h"
+
+
+
+namespace UiTbc
+{
+
+
+
+class UiOpenGLShader
+{
+public:
+    UiOpenGLShader();
+    ~UiOpenGLShader();
+
+    bool Build(const str& pVertexShaderSource, const str& pFragmentShaderSource);
+    unsigned GetProgram() const;
+
+private:
+    void Release();
+    static unsigned Compile(GLenum type, const str& pFilename);
+    bool Link();
+
+    unsigned mFragmentShader;
+    unsigned mVertexShader;
+    unsigned mShaderProgram;
+
+    LOG_CLASS_DECLARE();
+};
+
+
+
+}
---
+++ UiTBC/Source/UiOpenGLShader.cpp
@@ -0,0 +1,130 @@
+
+// Author: Jonas Byström
+// Copyright (c) 2002-2009, Righteous Games
+
+
+
+#define GL_GLEXT_PROTOTYPES
+#include "../Include/UiOpenGLShader.h"
+
+
+
+namespace UiTbc
+{
+
+
+
+UiOpenGLShader::UiOpenGLShader():
+	mFragmentShader(0),
+	mVertexShader(0),
+	mShaderProgram(0)
+{
+}
+
+UiOpenGLShader::~UiOpenGLShader()
+{
+	Release();
+}
+
+void UiOpenGLShader::Release()
+{
+	if (mVertexShader)
+	{
+		::glDeleteShader(mVertexShader);
+		mVertexShader = 0;
+	}
+	if (mFragmentShader)
+	{
+		::glDeleteShader(mFragmentShader);
+		mFragmentShader = 0;
+	}
+	if (mShaderProgram)
+	{
+		::glDeleteProgram(mShaderProgram);
+		mShaderProgram = 0;
+	}
+}
+
+unsigned UiOpenGLShader::GetProgram() const
+{
+	return mShaderProgram;
+}
+
+bool UiOpenGLShader::Build(const str& pVertexShaderSource, const str& pFragmentShaderSource)
+{
+	mShaderProgram = ::glCreateProgram();
+
+	mVertexShader = Compile(GL_VERTEX_SHADER, pVertexShaderSource);
+	mFragmentShader = Compile(GL_FRAGMENT_SHADER, pFragmentShaderSource);
+	if (mVertexShader == 0 || mFragmentShader == 0)
+	{
+		mLog.AError("Error while compiling shaders");
+		Release();
+		return false;
+	}
+
+	::glAttachShader(mShaderProgram, mVertexShader);
+	::glAttachShader(mShaderProgram, mFragmentShader);
+
+	if (!Link())
+	{
+		Release();
+		return false;
+	}
+
+	return true;
+}
+
+unsigned UiOpenGLShader::Compile(GLenum type, const str& pSource)
+{
+	if (pSource.empty())
+	{
+		mLog.Errorf(_T("Shader source '%s' is empty!"), pSource.c_str());
+		return 0;
+	}
+
+	GLuint lShader = ::glCreateShader(type);
+	astr lAnsiSource = astrutil::Encode(pSource);
+	const char* lRawSource = lAnsiSource.c_str();
+	::glShaderSource(lShader, 1, &lRawSource, 0);
+	::glCompileShader(lShader);
+	GLint lStatus;
+	::glGetShaderiv(lShader, GL_COMPILE_STATUS, &lStatus);
+	if (!lStatus)
+	{
+		mLog.Errorf(_T("Failed to compile shader with contents: '%s'"), pSource.c_str());
+		GLint lLogLength;
+		::glGetShaderiv(lShader, GL_INFO_LOG_LENGTH, &lLogLength);
+		if (lLogLength > 0)
+		{
+			unsigned char* lRawLog = new unsigned char[lLogLength];
+			::glGetShaderInfoLog(lShader, lLogLength, &lLogLength, (GLchar*)&lRawLog[0]);
+			astr lCharLog((char*)lRawLog);
+			str lLog = strutil::Encode(lCharLog);
+			mLog.Error(lLog);
+			delete lRawLog;
+		}
+		return 0;
+	}
+	return lShader;
+}
+
+bool UiOpenGLShader::Link()
+{
+	::glLinkProgram(mShaderProgram);
+	GLint lStatus;
+	::glGetProgramiv(mShaderProgram, GL_LINK_STATUS, &lStatus);
+	if (!lStatus)
+	{
+		mLog.AError("Failed to link shader program");
+	}
+	return lStatus? true : false;
+}
+
+
+
+LOG_CLASS_DEFINE(UI_GFX_3D, UiOpenGLShader);
+
+
+
+}
